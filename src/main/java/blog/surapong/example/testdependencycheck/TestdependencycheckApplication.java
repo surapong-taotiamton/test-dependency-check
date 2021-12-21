0a7f23e44a8086ff11501e7f306ccb7d92c46ff6
surapong <surapong.taotiamton@gmail.com>
2021-12-21 09:09:20 +0700

    Init project

---
+++ src/main/java/blog/surapong/example/testdependencycheck/TestdependencycheckApplication.java
@@ -0,0 +1,13 @@
+package blog.surapong.example.testdependencycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdependencycheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdependencycheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blog/surapong/example/testdependencycheck/TestdependencycheckApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.surapong.example.testdependencycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdependencycheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
